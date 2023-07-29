5e65dd958a88851ec4eea46ccf43aebd675fec8c
yagni711 <patelyagni5@gmail.com>
2023-07-29 18:52:19 +0530

    add by yagni

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Anglar</title>
+<link rel="icon" type="image/x-icon" href="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png">
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css"  integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU"  crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+<div class="background-sky hero"></div>
+<div class="menu">
+<div>
+	<img src="https://angular.io/assets/images/logos/angular/logo-nav@2x.png" class="divimg" style="z-index:2;">
+</div>
+<div  style="z-index:17;" class="divicon"><i class="fa fa-bars divicon"></i></div>
+	<div style="z-index:3;margin-left: 10px;" class="menu_name">DOCS</div>
+	<div style="z-index:4;" class="menu_name">COMMUNITY</div>
+	<div style="z-index:5;" class="menu_name">BLOG  <i class="fa fa-external-link" aria-hidden="true"></i></div>
+	<div class="menu_search"><a style="color:gray;font-size: 15px;">Search</a> <a class="searchbar">/</a></div>
+	<!-- <div style="z-index:7;margin-top:20px;margin-left:30px;"><i class="fa-solid fa-moon" style="color:#ffffff;"></i></div> -->
+	
+	<div style="z-index:7;" class="menu_right vertical"> <i class='fas fa-moon'></i> </div>
+	<div style="z-index:8;" class="menuright"><i class="fa fa-twitter" aria-hidden="true"></i></div>
+<div style="z-index:9;" class="github"><i class="fa fa-github" aria-hidden="true"></i></div>
+<div style="z-index:10;" class="youtube"><i class="fa fa-youtube-play" aria-hidden="true"></i></div>
+</div>
+	<div>
+		<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png" class="main_img">
+	</div>
+	<div>
+		<p class="text1"><b>Deliver web apps <br> with confidence</b></p>
+
+		<div class="tryangular"><center><b>TRY ANGULAR</b></center></div>
+	</div>
+<div class="text2"><b>
+	The web development framework for building the future</b>
+</div>
+<div class="text3">
+	<div class="lastmenu"><b>WORKS AT ANY SCALE</b></div>
+	<div class="lastmenu"><b>LOVED BY MILLIONS</b></div>
+	<div class="lastmenu"><b>BUILD FOR EVERYONE</b></div>
+</div>
+</body>
+</html>
+
